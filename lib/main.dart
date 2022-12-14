0d9bf8de956c29b22ddd654b5827b98984ba1c9b
YaelBusser <mrmaclevine@gmail.com>
2022-12-14 14:36:51 +0100

    Ajout fichiers

---
+++ lib/main.dart
@@ -0,0 +1,165 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Découverte de Flutter',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.purple,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      // This call to setState tells the Flutter framework that something has
+      // changed in this State, which causes it to rerun the build method below
+      // so that the display can reflect the updated values. If we changed
+      // _counter without calling setState(), then the build method would not be
+      // called again, and so nothing would appear to happen.
+      _counter++;
+    });
+  }
+  void _deCrementCounter() {
+    setState(() {
+      _counter--;
+    });
+  }
+
+  void _resetCounter(){
+    setState(() {
+        _counter = 0;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return Scaffold(
+      appBar: AppBar(
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text(widget.title),
+      ),
+      body: Align(
+        alignment: Alignment.bottomRight,
+        child :
+        Container(
+        child: ListView(
+          children: <Widget>[
+            const Text(
+              'Nombre :',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+            Align(
+              alignment: Alignment.topCenter,
+              child: Image.network("https://i.pinimg.com/736x/ce/28/24/ce28247e6760357c77c94401c6429995.jpg", width: 500, height: 500),
+            ),
+            Align(
+              alignment: Alignment.centerLeft,
+              child: Image.asset("images/DEYROS.png", width: 250, height: 250),
+            ),
+          ]
+        )
+      )
+      ),
+      floatingActionButton: Stack(
+      children: <Widget>[
+        Padding(padding: EdgeInsets.only(left:31),
+        child: Align(
+          alignment: Alignment.bottomLeft,
+          child: FloatingActionButton(
+            onPressed: _deCrementCounter,
+            child: Icon(Icons.exposure_minus_1),),
+        ),),
+
+        Align(
+          alignment: Alignment.bottomRight,
+          child: FloatingActionButton(
+            onPressed: _incrementCounter,
+          child: Icon(Icons.plus_one),),
+        ),
+        Padding(padding: EdgeInsets.only(left: 30),
+        child : Align(
+          alignment: Alignment.bottomCenter,
+          child: FloatingActionButton(
+            onPressed: _resetCounter,
+          child: Text('Reset'),),
+        ),
+        ),
+      ],
+    ),
+bottomNavigationBar: NavigationBar(
+        onDestinationSelected: (int index) {
+          setState(() {
+        });
+        },
+        destinations: const <Widget>[
+          NavigationDestination(
+            icon: Icon(Icons.home),
+            label: "Accueil",
+          ),
+          NavigationDestination(
+            icon: Icon(Icons.skip_next),
+            label: "Page suivante",
+          ),
+          NavigationDestination(
+            selectedIcon: Icon(Icons.bookmark),
+            icon: Icon(Icons.settings),
+            label: "Paramètres",
+          ),
+        ],
+      ),
+
+    );
+  }
+}
